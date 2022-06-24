9ae016c0901b5ff7a0e4f4723bdff8429c01c877
shaswat178 <shaswat178@gmail.com>
2022-06-24 16:04:13 +0530

    Tweet to feature script

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+set -e
+
 pip install emoji
 pip install transformers
 pip install tabulate
@@ -9,7 +11,7 @@ wget https://ndownloader.figshare.com/articles/6392078/versions/1
 unzip ./1
 tar -xf ./PHEME_veracity.tar.bz2
 
-mkdir logger
+mkdir -p logger
 
 python ./data_prep_3label.py ./all-rnr-annotated-threads ./data/pheme9/3label > ./logger/3labelprep.txt
 python ./main_3label.py ./data/pheme9/3label/unaugmented ./data/pheme9/3label ./model/pheme9/3label/unaugmented/ GCN 100 > ./logger/unaugmented_3label_gcn.txt
